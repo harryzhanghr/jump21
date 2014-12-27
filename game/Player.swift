caa4cc6a869201ae3ac4d72b18c03eeb88ca76a1
Harry Zhang <haoranzhang@berkeley.edu>
2014-12-27 08:26:28 +0800

    first commit with arguments bug

---
+++ game/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  game
+//
+//  Created by haoran zhang on 12/26/14.
+//  Copyright (c) 2014 haoran zhang. All rights reserved.
+//
+
+import Foundation
---
+++ game/Side.swift
@@ -0,0 +1,9 @@
+//
+//  Side.swift
+//  game
+//
+//  Created by haoran zhang on 12/26/14.
+//  Copyright (c) 2014 haoran zhang. All rights reserved.
+//
+
+import Foundation
